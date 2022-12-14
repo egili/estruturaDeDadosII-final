20a03ca3dfc8b4e15e4c396fde518c96a605074d
eliseu gili <gili.eliseup@gmail.com>
2022-12-14 19:31:12 -0300

    esqueci de commitar ontem

---
+++ src/FilaPrioridade.java
@@ -0,0 +1,37 @@
+public class FilaPrioridade<T> {
+	
+	
+
+	
+	public FilaPrioridade() {
+		this(10);
+	}
+	
+	public FilaPrioridade(int capacidadeInicial) {
+		
+	}
+	
+	public void adicionar() {
+		
+	}
+	
+	public T remover() {
+		
+	}
+	
+	
+	@Override
+	public String toString() {
+		
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		
+	}
+}
